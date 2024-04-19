acc585a548ccb54c0e1b75aa265b98a23bc7c590
Aditya-Singh-5014 <Aditya.singh.050204@gmail.com>
2024-04-19 21:48:27 +0530

    for hashrouting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import SignIn from "./Components/SignIn/SignIn";
 import Quiz from "./Components/Quiz/Quiz";
